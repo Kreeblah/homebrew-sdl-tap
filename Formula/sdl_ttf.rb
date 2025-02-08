fc8e08d01e71eb31c8d81a35564999b24f38ee59
Kreeblah <kreeblah@gmail.com>
2025-02-07 22:20:11 -0800

    Switch sdl_net to HEAD

--- Formula/sdl_ttf.rb
+++ Formula/sdl_ttf.rb
@@ -1,19 +1,10 @@
 class SdlTtf < Formula
   desc "Library for using TrueType fonts in SDL applications"
   homepage "https://github.com/libsdl-org/SDL_ttf"
-  revision 2
-
-  stable do
-    url "https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11.tar.gz"
-    sha256 "724cd895ecf4da319a3ef164892b72078bd92632a5d812111261cde248ebcdb7"
-
-    # Fix broken TTF_RenderGlyph_Shaded()
-    # https://bugzilla.libsdl.org/show_bug.cgi?id=1433
-    patch do
-      url "https://gist.githubusercontent.com/tomyun/a8d2193b6e18218217c4/raw/8292c48e751c6a9939db89553d01445d801420dd/sdl_ttf-fix-1433.diff"
-      sha256 "4c2e38bb764a23bc48ae917b3abf60afa0dc67f8700e7682901bf9b03c15be5f"
-    end
-  end
+  url "https://github.com/libsdl-org/SDL_ttf.git",
+      revision: "3c4233732b94ce08d5f6a868e597af39e13f8b23"
+  version "2.0.12-HEAD"
+  license "Zlib"
 
   head do
     url "https://github.com/libsdl-org/SDL_ttf.git", branch: "SDL-1.2"
