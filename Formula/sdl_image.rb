f4f9cd39841c9694b8d28eeb1d877804ed123fad
Kreeblah <kreeblah@gmail.com>
2025-02-07 20:38:13 -0800

    Fix sdl_image

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -1,21 +1,10 @@
 class SdlImage < Formula
   desc "Image file loading library"
   homepage "https://github.com/libsdl-org/SDL_image"
+  url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
+  sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
   license "Zlib"
   revision 10
-
-  stable do
-    url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
-    sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
-
-    # Fix graphical glitching
-    # https://github.com/Homebrew/homebrew-python/issues/281
-    # https://trac.macports.org/ticket/37453
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/41996822/sdl_image/IMG_ImageIO.m.patch"
-      sha256 "c43c5defe63b6f459325798e41fe3fdf0a2d32a6f4a57e76a056e752372d7b09"
-    end
-  end
 
   head do
     url "https://github.com/libsdl-org/SDL_image.git", branch: "SDL-1.2"
@@ -31,6 +20,19 @@ class SdlImage < Formula
   depends_on "libtiff"
   depends_on "sdl12-compat"
   depends_on "webp"
+
+  # Fix graphical glitching
+  # https://github.com/Homebrew/homebrew-python/issues/281
+  # https://trac.macports.org/ticket/37453
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/41996822/sdl_image/IMG_ImageIO.m.patch"
+    sha256 "c43c5defe63b6f459325798e41fe3fdf0a2d32a6f4a57e76a056e752372d7b09"
+  end
+
+  patch do
+    url "https://github.com/libsdl-org/SDL_image/commit/019f68f9f9460bdc37e5098d360ebc85758cae5c.diff"
+    sha256 "ccc6631adfd10f4265156cf88666bea974aa304996d2efcebf3ade3074312a9f"
+  end
 
   def install
     inreplace "SDL_image.pc.in", "@prefix@", HOMEBREW_PREFIX
