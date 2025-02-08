578d863b266d81bd98e5d5bcaf2de72daa913f2d
Kreeblah <kreeblah@gmail.com>
2025-02-07 21:55:00 -0800

    Switch sdl_sound to HEAD

--- Formula/sdl_sound.rb
+++ Formula/sdl_sound.rb
@@ -1,10 +1,10 @@
 class SdlSound < Formula
   desc "Library to decode several popular sound file formats"
   homepage "https://icculus.org/SDL_sound/"
-  url "https://icculus.org/SDL_sound/downloads/SDL_sound-1.0.3.tar.gz"
-  mirror "https://deb.debian.org/debian/pool/main/s/sdl-sound1.2/sdl-sound1.2_1.0.3.orig.tar.gz"
-  sha256 "3999fd0bbb485289a52be14b2f68b571cb84e380cc43387eadf778f64c79e6df"
-  revision 2
+  url "https://github.com/icculus/SDL_sound.git",
+      revision: "e525b7b4cbcb160e1d0392917d05df943608cc35"
+  version "1.0.4-HEAD"
+  license "LGPL-2.1-only"
 
   head do
     url "https://github.com/icculus/SDL_sound.git", branch: "stable-1.0"
@@ -20,11 +20,6 @@ class SdlSound < Formula
   depends_on "libogg"
   depends_on "libvorbis"
   depends_on "sdl12-compat"
-
-  patch do
-    url "https://github.com/icculus/SDL_sound/commit/11acb6cd3a7744dc6f0383aa6ef0fe1a531b99aa.diff"
-    sha256 "a28821936566129035441dd976e8863eba49bf3240413a8c7c80eaf5342ba4cb"
-  end
 
   def install
     system "./autogen.sh" if build.head?
