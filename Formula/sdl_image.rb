20c014410ddd8e318d48b5819bc09390ee5a71d7
Kreeblah <kreeblah@gmail.com>
2025-02-07 22:05:40 -0800

    Switch sdl_image to HEAD

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -1,17 +1,13 @@
 class SdlImage < Formula
   desc "Image file loading library"
   homepage "https://github.com/libsdl-org/SDL_image"
-  url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
-  sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
+  url "https://github.com/libsdl-org/SDL_image.git",
+      revision: "3ae305b63db52b753333141d9536d23815a9f97f"
+  version "1.2.13-HEAD"
   license "Zlib"
-  revision 10
 
   head do
     url "https://github.com/libsdl-org/SDL_image.git", branch: "SDL-1.2"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "pkgconf" => :build
@@ -20,21 +16,6 @@ class SdlImage < Formula
   depends_on "libtiff"
   depends_on "sdl12-compat"
   depends_on "webp"
-
-  patch do
-    url "https://github.com/libsdl-org/SDL_image/commit/019f68f9f9460bdc37e5098d360ebc85758cae5c.diff"
-    sha256 "ccc6631adfd10f4265156cf88666bea974aa304996d2efcebf3ade3074312a9f"
-  end
-
-  patch do
-    url "https://github.com/libsdl-org/SDL_image/commit/1c1755efa690e0998a33b6ae4af1bb526f5a6e2c.diff"
-    sha256 "21af348fb1ac5609fb63cdfbf7850d4d6407b4a5809a408f8e26802958ef84e0"
-  end
-
-  patch do
-    url "https://github.com/libsdl-org/SDL_image/commit/abb2c39f0326bd5ec3ebde314907c71a8487e997.diff"
-    sha256 "1d496f6817ccf5a8e5b63ae135e0a8ee09cbefdcabcf5115d4452d9fb82574db"
-  end
 
   def install
     inreplace "SDL_image.pc.in", "@prefix@", HOMEBREW_PREFIX
